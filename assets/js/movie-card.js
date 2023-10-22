53376a5186a95679c1002b202b562038bbc8cce9
ianmusasiah <115050114+ianmusasiah@users.noreply.github.com>
2023-10-23 01:36:24 +0300

    Update movie-card.js

--- assets/js/movie-card.js
+++ assets/js/movie-card.js
@@ -37,7 +37,8 @@ export function createMovieCard(movie) {
       <div class="card-badge">${release_date.split("-")[0]}</div>
     </div>
     
-    <a href="./detail.html" class="card-btn" title="${title}" onclick="getMovieDetail(${id})"></a>
+    <a href="./detail.html?${id}/${encodeURIComponent(title.replace(/ /g, '-').toLowerCase())}" class="card-btn" title="${title}" onclick="getMovieDetail(${id})"></a>
+
   `;
 
   return card;
