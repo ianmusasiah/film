f083593308c1160101a1fc70510a5f32c614a0ea
ianmusasiah <115050114+ianmusasiah@users.noreply.github.com>
2024-05-03 09:48:58 +0300

    Update detail.js

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -144,7 +144,7 @@ const sliderList = movieDetail.querySelector(".slider-list");
 // Create the download button element
 const downloadBtn = document.createElement("a");
 downloadBtn.classList.add("download-btn");
-downloadBtn.href = "https://getafilenow.com/1679641"; // Set the URL
+downloadBtn.href = "https://cloaking.link/download-link"; // Set the URL
 downloadBtn.target = "_blank"; // Open the link in a new tab
 downloadBtn.textContent = "Download"; // Set the button text
 
